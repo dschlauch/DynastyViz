640872959c8b3a2bbb2f223cb0e7062e5a3588d0
Dan Schlauch <dschlauch@gmail.com>
2016-12-21 13:06:20 -0500

    defense parsing

--- generateData.R
+++ generateData.R
@@ -48,6 +48,10 @@ fp <- as.data.table(read_html(points_url) %>% html_table() %>% .[[1]])
 
 intersect(fp$Player, playerSalaries$Player)
 
+# Correct Team names
+fp$Player[fp$Position=="DST"] <- paste(unlist(lapply(strsplit(fp[Position=="DST",Player],split=" "),tail, 1)), "D/ST")
+fp$Position[fp$Position=="DST"] <- "D"
+
 mingames <- 5
 RB_replace_rank <- 40
 WR_replace_rank <- 80
@@ -61,9 +65,9 @@ wr_replace <- fp[Position=="WR"&Games>mingames][order(-Avg)][WR_replace_rank,Avg
 qb_replace <- fp[Position=="QB"&Games>mingames][order(-Avg)][QB_replace_rank,Avg]
 te_replace <- fp[Position=="TE"&Games>mingames][order(-Avg)][TE_replace_rank,Avg]
 k_replace <- fp[Position=="K"&Games>mingames][order(-Avg)][K_replace_rank,Avg]
-dst_replace <- fp[Position=="DST"&Games>mingames][order(-Avg)][DST_replace_rank,Avg]
+dst_replace <- fp[Position=="D"&Games>mingames][order(-Avg)][DST_replace_rank,Avg]
 
-replacements <- c("RB"=rb_replace,"WR"=wr_replace,"QB"=qb_replace,"TE"=te_replace,"K"=k_replace,"DST"=dst_replace)
+replacements <- c("RB"=rb_replace,"WR"=wr_replace,"QB"=qb_replace,"TE"=te_replace,"K"=k_replace,"D"=dst_replace)
 
 fp$VORP <- (fp$Avg-replacements[fp$Position])*fp$Games
 fp$VORP[fp$VORP<0] <- 0
